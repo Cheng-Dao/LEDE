c9ef07ddf692750b683719686e951226c2e28845
silime <60662669+silime@users.noreply.github.com>
2020-02-05 22:33:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,9 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-rm -rf /package/lean/luci-theme-argon
+#rm -rf /package/lean/luci-theme-argon
 https://github.com/destan19/OpenAppFilter.git /package/OpenAppFilter
-git clone https://github.com/jerrykuku/luci-theme-argon.git /package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git /package/lean/luci-theme-argon
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/silime/luci-app-mentohust.git package/luci-app-mentohust
 git clone https://github.com/openwrt-dev/po2lmo.git
