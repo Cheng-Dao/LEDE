6988263b9e38e3bd5a0c3cdf7f9edf8eabc53bdb
silime <60662669+silime@users.noreply.github.com>
2020-02-05 19:05:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 rm -rf /package/lean/luci-theme-argon
 https://github.com/destan19/OpenAppFilter.git /package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-theme-argon.git /package/lean/luci-theme-argon
