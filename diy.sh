39926c31b2aed1332bed1d0ef2f3357f74cadb3c
silime <60662669+silime@users.noreply.github.com>
2020-02-07 01:07:18 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,24 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/OpenWrt/Silime/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo '修改默认主题'
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+echo '去除默认bootstrap主题'
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+echo '删除旧版argon,链接新版'
+rm -rf ./package/lean/luci-theme-argon
+ln -s ../../../luci-theme-argon ./package/lean/
+
+echo '修改wifi名称'
+sed -i 's/OpenWrt/Silime/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #rm -rf /package/lean/luci-theme-argon
 https://github.com/destan19/OpenAppFilter.git /package/OpenAppFilter
 #git clone https://github.com/jerrykuku/luci-theme-argon.git /package/lean/luci-theme-argon
