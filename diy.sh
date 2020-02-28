f9779ccefe411a18f3b7631e1490d7f0de4b9d87
silime <60662669+silime@users.noreply.github.com>
2020-02-29 04:04:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,6 +5,9 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib linux-libc-dev:i386
+
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
